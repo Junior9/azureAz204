e6050dff91df9d00c68d724457fe2458dffde6a9
Junior9 <moraes.junior6@gmail.com>
2024-12-09 15:14:13 -0600

    Azure Storage blob upload

--- src/main/java/com/azure204/cloud/controller/AdvertController.java
+++ src/main/java/com/azure204/cloud/controller/AdvertController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.azure.cosmos.implementation.HttpConstants.StatusCodes;
 import com.azure.cosmos.util.CosmosPagedIterable;
 import com.azure204.cloud.model.Advert;
 import com.azure204.cloud.service.AdvertService;
 import com.azure204.cloud.service.ChatGtpService;
+import com.azure204.cloud.service.UploadService;
 
 import lombok.AllArgsConstructor;
 
@@ -25,6 +28,7 @@ public class AdvertController {
 
     private AdvertService advertService;
     private ChatGtpService chatGtpService;
+    private UploadService uploadService;
 
     @GetMapping("/advert")
     public CosmosPagedIterable<Object> getAdverts(){
@@ -47,9 +51,10 @@ public class AdvertController {
         return ResponseEntity.status(StatusCodes.CREATED).body(advertCreated);
     }
 
-    @PostMapping("/upload/imagine")
-    public void uploadImagine(@RequestBody Advert advert){
-        this.advertService.addAdvert(advert);
+    @GetMapping("/upload/imagine")
+    public ResponseEntity<String>  uploadImagine(@RequestParam MultipartFile data,@RequestParam String userId, @RequestParam String advertId){
+        String url = this.uploadService.uploadBlob(data, userId , advertId);
+        return ResponseEntity.ok(url);
     }
 
     @PutMapping("/advert")
--- src/main/java/com/azure204/cloud/service/UploadService.java
+++ src/main/java/com/azure204/cloud/service/UploadService.java
@@ -1,13 +1,42 @@
 package com.azure204.cloud.service;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure204.cloud.common.KeyVault;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class UploadService {
 
+    private BlobServiceClient blobServiceClient;
+    private KeyVault keyVault;
 
-    public void uploadBlob(){
-        
+    @PostConstruct
+    public void init(){
+        this.keyVault = new KeyVault();
+        this.blobServiceClient = new BlobServiceClientBuilder().connectionString(this.keyVault.getSecret("storageblobConnectString")).buildClient();
+    }
+
+    public String uploadBlob(MultipartFile data, String userId, String advertsId) {
+        try {
+            String blobName = data.getOriginalFilename();
+            BlobClient storageBlobClient = this.blobServiceClient.getBlobContainerClient("adverts").getBlobClient(blobName);
+            storageBlobClient.upload(data.getInputStream(),data.getSize(), true);
+            String url = storageBlobClient.getBlobUrl();
+            return url;
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "";
+        }
     }
 
 }
