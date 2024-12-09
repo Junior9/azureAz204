32c8f061b71584b5d14d898d74f80c173c88b81b
Junior9 <moraes.junior6@gmail.com>
2024-12-09 10:53:11 -0600

    Fix break build

--- src/main/java/com/azure204/cloud/controller/AdvertController.java
+++ src/main/java/com/azure204/cloud/controller/AdvertController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.azure.cosmos.implementation.HttpConstants.StatusCodes;
 import com.azure.cosmos.util.CosmosPagedIterable;
-import com.azure204.cloud.dto.GenerateAiDTO;
 import com.azure204.cloud.model.Advert;
 import com.azure204.cloud.service.AdvertService;
 import com.azure204.cloud.service.ChatGtpService;
@@ -45,8 +44,12 @@ public class AdvertController {
     @GetMapping("/ai/generate/{msn}")
     public ResponseEntity<Advert> generateTextAI(@PathVariable String msn) throws URISyntaxException{
         Advert advertCreated = this.chatGtpService.generate(msn);
-
         return ResponseEntity.status(StatusCodes.CREATED).body(advertCreated);
+    }
+
+    @PostMapping("/upload/imagine")
+    public void uploadImagine(@RequestBody Advert advert){
+        this.advertService.addAdvert(advert);
     }
 
     @PutMapping("/advert")
---
+++ src/main/java/com/azure204/cloud/service/UploadService.java
@@ -0,0 +1,13 @@
+package com.azure204.cloud.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UploadService {
+
+
+    public void uploadBlob(){
+        
+    }
+
+}
